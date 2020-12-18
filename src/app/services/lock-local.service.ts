4c34580174710d032056e4f360ea4f524e8c97fe
marfrede <marvin.frede@hs-osnabrueck.de>
2020-12-18 14:02:43 +0100

    locla lock service tries to initiate locks from lcoks.json. if this file is not found it uses the file locks.example.json after showing a warning message

--- src/app/services/lock-local.service.ts
+++ src/app/services/lock-local.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Lock } from '../models/lock';
 import { Observable } from 'rxjs';
 
+const LOCKFILE = '../../assets/locks/locks.json';
+const LOCKFILEEXAMPLE = '../../assets/locks/locks.example.json';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,31 +14,70 @@ import { Observable } from 'rxjs';
  */
 export class LockLocalService {
 
+  public searchingLocks: boolean; // currently unused => show loading spinner while true
+  public foundLocks: boolean;
+  public message: string;
+
   private locks: Lock[] = null;
+  private http: HttpClient;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(httpClient: HttpClient) {
+    this.http = httpClient;
+  }
 
-  private async initLocks(): Promise<void> {
-    console.log('initiating local locks...');
+  private async initLocks(path: string): Promise<void> {
+    this.message = null;
+    this.foundLocks = false;
+    this.searchingLocks = true;
     return new Promise<void>((resolve) => {
-      this.readLocksFromJson().subscribe((locks) => {
-        this.locks = locks;
-        this.locks.forEach((lock: Lock) => {
-          lock = this.convertSecretAndPasswordToHexadecimal(lock);
-          lock = this.addNameToLock(lock, 'OKGSS101');
-        });
-        console.log('...initiated local locks!');
+      this.readLocksFromJson(path).subscribe((locks) => {
+        this.processLocks(locks);
         resolve();
+      }, error => {
+        this.processError(error);
+        setTimeout(_ => resolve(this.initLocks(LOCKFILEEXAMPLE)), 10000); // show message before getting example locks
       });
     });
+  }
+
+  private processLocks(locks: Lock[]) {
+    this.foundLocks = true;
+    this.searchingLocks = false;
+    this.locks = locks;
+    this.locks.forEach((lock: Lock) => {
+      lock = this.convertSecretAndPasswordToHexadecimal(lock);
+      lock = this.addNameToLock(lock, 'OKGSS101');
+    });
+    console.log('...initiated local locks!');
+  }
+
+  private processError(error: any) {
+    this.foundLocks = false;
+    this.searchingLocks = false;
+    switch (error.status) {
+      case 404:
+        this.message = `ERROR<hr>COULD NOT FIND THE FILE ${LOCKFILE}<br>FOR MORE INFOS OPEN THE BROWSERS CONSOLE.<br>LOADING LOCK EXAMPLES....`;
+        console.error(`COULD NOT FIND THE FILE ${LOCKFILE}! => NOW LOADING LOCK EXAMPLES. For more infos look below.`);
+        console.error('DE: Du musst eine Datei names \'locks.json\' innerhalb des Ordners \'/src/assets/locks\' erstellen.');
+        console.error('DE: Kopiere für den Anfang den Inhalt der Datei \'locks.example.json\' in die Datei \'locks.json\' hinein.');
+        console.error('DE: Wenn du ein Schloss / Schlösser besitzt, füge die Daten in die \'locks.json\' Datei ein.');
+        console.error('DE: Baue oder starte die App danach neu (ionic serve/build).');
+        console.error('EN: You have to create a file named \'locks.json\' inside the folder \'/src/assets/locks\'.');
+        console.error('EN: To get started, copy the contents of the \'locks.example.json\' file into \'locks.json\'.');
+        console.error('EN: If you have a lock / locks, add the data into the \'locks.json\' file.');
+        console.error('EN: Rebuild / Restart the app afterwards (ionic serve/build).');
+        break;
+      default:
+        console.log('UNKNWON ERROR OCCURED');
+        break;
+    }
+    return;
   }
 
   public async getLocks(): Promise<Lock[]> {
     if (!this.locks) {
       console.log('waiting');
-      await this.initLocks();
+      await this.initLocks(LOCKFILE);
     }
     return this.locks;
   }
@@ -43,13 +85,13 @@ export class LockLocalService {
   public async getLock(id: Readonly<string>): Promise<Lock> {
     if (!this.locks) {
       console.log('waiting');
-      await this.initLocks();
+      await this.initLocks(LOCKFILE);
     }
     return this.locks.find(lock => lock.id === id);
   }
 
-  private readLocksFromJson(): Observable<Lock[]> {
-    return this.http.get<Lock[]>('../../assets/locks/locks.json');
+  private readLocksFromJson(path: string): Observable<Lock[]> {
+    return this.http.get<Lock[]>(path);
   }
 
   private addNameToLock(lock: Readonly<Lock>, name: Readonly<string>) {
